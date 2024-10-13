8d6267097c42d5ea1cd719112333fdc65abea028
Abulkosim <abulkasimkattaev@gmail.com>
2024-10-13 20:55:41 +0500

    Project structure

---
+++ index.js
@@ -0,0 +1,34 @@
+import { parseArgs } from 'node:util';
+import { homedir } from 'os';
+import { createInterface } from 'readline/promises';
+import { handleCommand } from './src/cli';
+
+const { values } = parseArgs({
+  options: {
+    username: { type: 'string' }
+  }
+});
+
+const username = values.username || 'Username';
+const homeDir = homedir();
+
+console.log(`Welcome to the File Manager, ${username}!`);
+console.log(`You are currently in ${homeDir}`);
+
+const rl = createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+process.on('exit', () => {
+  console.log(`Thank you for using File Manager, ${username}!`);
+});
+
+while (true) {
+  const input = await rl.question('> ');
+  if (input.trim() === '.exit') {
+    process.exit(0);
+  }
+  await handleCommand(input);
+  console.log(`You are currently in ${process.cwd()}`);
+}
