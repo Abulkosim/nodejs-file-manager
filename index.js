623bcec690a9a6b19ef027b5ca3473877feb2a22
Abulkosim <abulkasimkattaev@gmail.com>
2024-10-14 12:45:31 +0500

    fix: change to home directory

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import { parseArgs } from 'node:util';
 import { homedir } from 'os';
 import { createInterface } from 'readline/promises';
 import { handleCommand } from './src/cli.js';
+import { chdir } from 'process';
 
 const { values } = parseArgs({
   options: {
@@ -15,13 +16,15 @@ const homeDir = homedir();
 console.log(`Welcome to the File Manager, ${username}!`);
 console.log(`You are currently in ${homeDir}`);
 
+chdir(homeDir);
+
 const rl = createInterface({
   input: process.stdin,
   output: process.stdout
 });
 
 process.on('exit', () => {
-  console.log(`Thank you for using File Manager, ${username}!`);
+  console.log(`Thank you for using File Manager, ${username}, goodbye!`);
 });
 
 while (true) {
